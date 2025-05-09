b9004845c6764914adeda0554a681fa5f937f480
AryaaRathore <aryarathore835@gmail.com>
2025-05-09 20:52:09 +0530

    second commit

---
+++ reverse.java
@@ -0,0 +1,13 @@
+public class reverse {
+    public static void main(String[] args) {
+        int[] a={2,4,6,8,10};
+        int n=5;
+        for(int  Qi=n-1;i>=0;i--){
+            System.out.println("reverse array "+a[i]);
+
+        }
+        }
+    
+    }
+    
+
