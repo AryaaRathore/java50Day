5f938c4f8a0149d0c7859f5bdf24a66efedf40f7
AryaaRathore <aryarathore835@gmail.com>
2025-05-26 22:25:07 +0530

    subsequence

---
+++ subsequence.java
@@ -0,0 +1,21 @@
+public class subsequence {
+    public static void main(String[] args) {
+        String str1="COMPUTER";
+        String str2="CUTER";
+        int i=0,j=0;    
+        while(i<str1.length()&&j<str2.length()){
+            if(str1.charAt(i)==str2.charAt(j)){
+                i++;
+            }
+            j++;
+        }
+        if(j==str2.length()){
+            System.out.println("str2 is a subsequence of str1");
+        }
+        else{
+            System.out.println("str1 is not a subsequence of str2");
+        }
+    }
+}
+    
+
