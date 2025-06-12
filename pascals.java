503b0cff4dfbe13f5c0ba84ebe4167f16110183e
AryaaRathore <aryarathore835@gmail.com>
2025-06-12 22:36:53 +0530

    pascal tringle

---
+++ pascals.java
@@ -0,0 +1,26 @@
+public class pascals {
+    
+    public static void main(String[] args) {
+        int num=1;
+        for(int i=0;i<=5;i++){
+            for(int j=0;j<=5-i;j++){
+                System.out.print("  ");
+            }
+            for(int k=0;k<=i;k++){
+        
+                
+                System.out.printf("%4d",num);//4 use width
+                num=num*(i-k)/(k+1);//next number check
+            }
+            
+            System.out.println( );
+            num++;
+            
+        }
+    }
+    
+}
+
+
+    
+
