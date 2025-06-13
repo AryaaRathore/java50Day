196c8941c43506d72deae6da9193a1716a9286da
AryaaRathore <aryarathore835@gmail.com>
2025-06-13 21:17:43 +0530

    snake

--- pascals.java
+++ pascals.java
@@ -1,12 +1,16 @@
-public class pascals {
-    
-    public static void main(String[] args) {
+public class pascals
+ {
+    public static void main(String[] args)
+     {
         int num=1;
-        for(int i=0;i<=5;i++){
-            for(int j=0;j<=5-i;j++){
+        for(int i=0;i<=5;i++)
+        {
+            for(int j=0;j<=5-i;j++)
+            {
                 System.out.print("  ");
             }
-            for(int k=0;k<=i;k++){
+            for(int k=0;k<=i;k++)
+            {
         
                 
                 System.out.printf("%4d",num);//4 use width
